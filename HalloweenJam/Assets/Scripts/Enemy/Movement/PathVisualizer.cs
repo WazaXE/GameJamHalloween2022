3994cda93dbde80c5e6133971eab5fb4b11d44ed
Mogge3 <morgan.sponton@outlook.com>
2022-10-29 21:36:26 +0200

    pathBuilder uppdatering

--- HalloweenJam/Assets/Scripts/Enemy/Movement/PathVisualizer.cs
+++ HalloweenJam/Assets/Scripts/Enemy/Movement/PathVisualizer.cs
@@ -6,15 +6,18 @@ public class PathVisualizer : MonoBehaviour
 {
     [SerializeField] private PatrolPath targetPath;
 
+    [SerializeField] private Color pointColor;
+    [SerializeField] private Color lineColor;
+
     private void OnDrawGizmos() {
         if (targetPath == null) return;
 
         for (int i = 0; i < targetPath.PatrolPoints.Length; i++) {
-            Gizmos.color = Color.blue;
+            Gizmos.color = pointColor;
             Gizmos.DrawSphere(targetPath.PatrolPoints[i], 0.2f);
 
             if (i < targetPath.PatrolPoints.Length - 1) {
-                Gizmos.color = Color.black;
+                Gizmos.color = lineColor;
                 Gizmos.DrawLine(targetPath.PatrolPoints[i], targetPath.PatrolPoints[i + 1]);
             }
         }
