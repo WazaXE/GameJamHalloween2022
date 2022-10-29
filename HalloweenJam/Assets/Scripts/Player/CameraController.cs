e9db488600f496a4fa1cce289132237fa7d94549
Mogge3 <morgan.sponton@outlook.com>
2022-10-29 11:32:59 +0200

    input axis

--- HalloweenJam/Assets/Scripts/Player/CameraController.cs
+++ HalloweenJam/Assets/Scripts/Player/CameraController.cs
@@ -10,7 +10,7 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         Debug.Log(Input.GetAxis("Horizontal Right"));
-        float rotationAmount = Input.GetAxis("Horizontal Right") * rotationSpeed;
+        float rotationAmount = Input.GetAxis("Horizontal Right") * rotationSpeed * Time.deltaTime;
         Vector3 rotation = rotationTarget.rotation.eulerAngles;
         rotation.y += rotationAmount;
         rotationTarget.rotation = Quaternion.Euler(rotation);
