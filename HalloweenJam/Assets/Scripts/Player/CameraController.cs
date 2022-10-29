3e95e1966ee3309282ae5ae7c6507271902525a0
Mogge3 <morgan.sponton@outlook.com>
2022-10-29 11:20:40 +0200

    camera rotation script + input axes

---
+++ HalloweenJam/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private Transform rotationTarget;
+
+    void Update()
+    {
+        Debug.Log(Input.GetAxis("Horizontal Right"));
+        float rotationAmount = Input.GetAxis("Horizontal Right") * rotationSpeed;
+        Vector3 rotation = rotationTarget.rotation.eulerAngles;
+        rotation.y += rotationAmount;
+        rotationTarget.rotation = Quaternion.Euler(rotation);
+    }
+}
